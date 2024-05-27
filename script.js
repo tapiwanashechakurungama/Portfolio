1a2ee03773e6d148d752017baea51cf4964e1bfd
Tapiwanashe Chakurungama <chakurungamatapiwanashe@gmail.com>
2024-05-27 10:52:09 +0200

    my commits

---
+++ script.js
@@ -0,0 +1,37 @@
+//navbar and hamburger menu//
+var icon = document.getElementById("humburger");
+let nav = document.getElementById("navbar");
+const body = document.body;
+const cv = document.getElementById("download");
+const link = document.querySelectorAll("a")
+
+icon.addEventListener('click', function(event){
+        event.preventDefault();
+        navbar.classList.toggle('active')
+});
+
+cv.addEventListener('click', function(e){
+        event.target.textContent = "Cv Downloaded"
+})
+ 
+nav.addEventListener("click", function(e){
+        navbar.classList.remove('active')
+})
+//nashy the compiller
+
+//maspellings haana basa
+
+// name = "Tapiwanashe"
+
+// print(len(name))
+
+// surname = "chakurungama"
+
+// print("f{name} and his surname is {surname}")
+
+// number1= 10;
+// number2 = 20;
+
+// if number1 > number2 :
+//      print('number1 is greater')
+// else print('number2 is greater')
